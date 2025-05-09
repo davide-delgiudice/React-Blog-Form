1cba5953d26fe2e096006cdf0d754715e9442d84
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-09 15:21:54 +0200

    [UPD] create videogames array and imported

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import videogames from '../data/videogames'
 
 function Main() {
   return (
---
+++ src/data/videogames.js
@@ -0,0 +1,34 @@
+const videogames = [
+    {
+        id: 1,
+        name: "The Witcher 3",
+        genre: "RPG"
+    },
+    {
+        id: 2,
+        name: "Undertale",
+        genre: "RPG"
+    },
+    {
+        id: 3,
+        name: "Portal",
+        genre: "Puzzle"
+    },
+    {
+        id: 4,
+        name: "Hollow Knight",
+        genre: "Metroidvania"
+    },
+    {
+        id: 5,
+        name: "DOOM: Eternal",
+        genre: "FPS"
+    },
+    {
+        id: 6,
+        name: "Street Fighter 6",
+        genre: "Picchiaduro"
+    },
+];
+
+export default videogames
