1aded83727d1bacd0b74971cbfb687b768402e42
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-09 17:50:44 +0200

    [UPD] add states and addGame function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,20 +3,36 @@ import videogames from '../data/videogames'
 import { useState } from 'react'
 
 function Main() {
+
+    const [games, setGames] = useState(videogames)
+    const [newGame, setNewGame] = useState('')
+
+    const addGame = (e) =>{
+        e.preventDefault();
+        setGames([...games, newGame]);
+        setNewGame('');
+    }
+
   return (
     <>
         <div className='container'>
             <ul className="list-group">
-                {videogames.map((videogame) => (
+                {games.map((videogame) => (
                     <li key={`videogame-${videogame.id}`} className="list-group-item">
                         {videogame.name}
                     </li>
                 ))}
             </ul>
 
-            <form>
+            <form onSubmit={addGame}>
                 <div className='input-group mt-5'>
-                    <input type="text" className='form-control' placeholder='Inserisci un nuovo gioco!'/>
+                    <input 
+                        type="text" 
+                        className='form-control' 
+                        placeholder='Inserisci un nuovo gioco!' 
+                        value={newGame} 
+                        onChange={(e) => setNewGame(e.target.value)}
+                    />
                     <button className='btn btn-success'>Aggiungi</button>
                 </div>
             </form>
